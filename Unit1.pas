1aa245aabe26ac0be2c990f74216f18498608c63
lucas.alemao123456@hotmail.com <kilerhg>
2020-07-16 09:06:48 -0300

    #2

--- Unit1.pas
+++ Unit1.pas
@@ -42,7 +42,6 @@ type
     Label1: TLabel;
     Button10: TButton;
     edtb: TEdit;
-    Button11: TButton;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
