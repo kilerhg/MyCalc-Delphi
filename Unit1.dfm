67871cc33466b94fed18fe85d47a1bc19fbeadcd
lucas.alemao123456@hotmail.com <kilerhg>
2020-07-16 07:49:02 -0300

    #1

---
+++ Unit1.dfm
@@ -0,0 +1,386 @@
+object principal: Tprincipal
+  Left = 1371
+  Top = 309
+  BiDiMode = bdLeftToRight
+  BorderStyle = bsSingle
+  Caption = 'Calculadora'
+  ClientHeight = 232
+  ClientWidth = 243
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  ParentBiDiMode = False
+  PopupMenu = PopupMenu1
+  Position = poScreenCenter
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label3: TLabel
+    Left = 109
+    Top = 94
+    Width = 15
+    Height = 25
+    Alignment = taCenter
+    AutoSize = False
+    BiDiMode = bdRightToLeftNoAlign
+    Caption = '='
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -21
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentBiDiMode = False
+    ParentFont = False
+  end
+  object Label4: TLabel
+    Left = 147
+    Top = 69
+    Width = 69
+    Height = 19
+    Caption = 'Resultado'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label6: TLabel
+    Left = 6
+    Top = 8
+    Width = 41
+    Height = 18
+    Caption = 'Data :'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label7: TLabel
+    Left = 53
+    Top = 8
+    Width = 5
+    Height = 18
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label8: TLabel
+    Left = 6
+    Top = 32
+    Width = 41
+    Height = 18
+    Caption = 'Hora :'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label9: TLabel
+    Left = 53
+    Top = 32
+    Width = 5
+    Height = 18
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -15
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label1: TLabel
+    Left = 53
+    Top = 54
+    Width = 12
+    Height = 13
+    Caption = 'n1'
+  end
+  object Button1: TButton
+    Left = 213
+    Top = 371
+    Width = 25
+    Height = 25
+    Caption = '+'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 0
+    Visible = False
+    OnClick = Button1Click
+  end
+  object edt1: TEdit
+    Left = 8
+    Top = 73
+    Width = 95
+    Height = 27
+    Alignment = taCenter
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 1
+    OnExit = edt1Exit
+  end
+  object edt2: TEdit
+    Left = 9
+    Top = 73
+    Width = 94
+    Height = 27
+    Alignment = taCenter
+    Color = clMenuBar
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 2
+    OnExit = edt2Exit
+  end
+  object Button2: TButton
+    Left = 237
+    Top = 371
+    Width = 25
+    Height = 25
+    Caption = '-'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 3
+    Visible = False
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 257
+    Top = 371
+    Width = 25
+    Height = 25
+    Caption = '/'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 4
+    Visible = False
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 278
+    Top = 371
+    Width = 25
+    Height = 25
+    Caption = '*'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -19
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 5
+    Visible = False
+    OnClick = Button4Click
+  end
+  object edtresult: TEdit
+    Left = 130
+    Top = 94
+    Width = 105
+    Height = 33
+    Alignment = taCenter
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -21
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    ReadOnly = True
+    TabOrder = 6
+  end
+  object Button6: TButton
+    Left = 228
+    Top = 456
+    Width = 75
+    Height = 25
+    Caption = '0 a 100'
+    TabOrder = 7
+    Visible = False
+    OnClick = Button6Click
+  end
+  object ComboBox1: TComboBox
+    Left = 8
+    Top = 106
+    Width = 95
+    Height = 27
+    AutoDropDown = True
+    BevelEdges = []
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 8
+    Text = 'Operador'
+    OnChange = ComboBox1Change
+    Items.Strings = (
+      '+'
+      '-'
+      '/'
+      '*'
+      #178#8730
+      'x'#178
+      '%')
+  end
+  object Button8: TButton
+    Left = 53
+    Top = 183
+    Width = 129
+    Height = 41
+    Caption = 'Calcular'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -27
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 9
+    OnClick = Button8Click
+  end
+  object Button7: TButton
+    Left = 231
+    Top = 197
+    Width = 75
+    Height = 25
+    Caption = 'Button7'
+    TabOrder = 10
+    Visible = False
+    OnClick = Button7Click
+  end
+  object Panel1: TPanel
+    Left = 20
+    Top = 236
+    Width = 196
+    Height = 417
+    Color = clWhite
+    Ctl3D = True
+    ParentBackground = False
+    ParentCtl3D = False
+    TabOrder = 11
+    object Label5: TLabel
+      Left = 42
+      Top = 3
+      Width = 103
+      Height = 33
+      BiDiMode = bdLeftToRight
+      Caption = 'Historico'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -27
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentBiDiMode = False
+      ParentFont = False
+    end
+    object Button5: TButton
+      Left = 17
+      Top = 384
+      Width = 153
+      Height = 25
+      Caption = 'Limpar Historico'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -19
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 0
+      OnClick = Button5Click
+    end
+    object Memo1: TMemo
+      Left = 11
+      Top = 42
+      Width = 173
+      Height = 313
+      ReadOnly = True
+      ScrollBars = ssVertical
+      TabOrder = 1
+    end
+    object ProgressBar1: TProgressBar
+      Left = 11
+      Top = 361
+      Width = 173
+      Height = 17
+      TabOrder = 2
+    end
+  end
+  object Button9: TButton
+    Left = 248
+    Top = 228
+    Width = 75
+    Height = 25
+    Caption = 'Button9'
+    TabOrder = 12
+    Visible = False
+    OnClick = Button9Click
+  end
+  object Button10: TButton
+    Left = 147
+    Top = 144
+    Width = 75
+    Height = 25
+    Caption = 'Button10'
+    TabOrder = 13
+    OnClick = Button10Click
+  end
+  object edtb: TEdit
+    Left = 8
+    Top = 150
+    Width = 94
+    Height = 27
+    TabOrder = 14
+    Visible = False
+  end
+  object Timer1: TTimer
+    OnTimer = Timer1Timer
+    Left = 230
+    Top = 408
+  end
+  object Timer2: TTimer
+    Interval = 100
+    OnTimer = Timer2Timer
+    Left = 270
+    Top = 408
+  end
+  object PopupMenu1: TPopupMenu
+    Left = 264
+    Top = 304
+    object EsconderHistorico1: TMenuItem
+      Caption = 'Esconder Historico'
+      OnClick = EsconderHistorico1Click
+    end
+    object MostrarHistorico1: TMenuItem
+      Caption = 'Mostrar Historico'
+      OnClick = MostrarHistorico1Click
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,342 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.ComCtrls,
+  Vcl.Menus, Math, System.UITypes;
+
+type
+  Tprincipal = class(TForm)
+    Button1: TButton;
+    edt1: TEdit;
+    edt2: TEdit;
+    Label3: TLabel;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    edtresult: TEdit;
+    Label4: TLabel;
+    Memo1: TMemo;
+    Label5: TLabel;
+    Button5: TButton;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Timer1: TTimer;
+    Button6: TButton;
+    Timer2: TTimer;
+    ProgressBar1: TProgressBar;
+    ComboBox1: TComboBox;
+    Button8: TButton;
+    Button7: TButton;
+    Panel1: TPanel;
+    Button9: TButton;
+    PopupMenu1: TPopupMenu;
+    EsconderHistorico1: TMenuItem;
+    MostrarHistorico1: TMenuItem;
+    Label1: TLabel;
+    Button10: TButton;
+    edtb: TEdit;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Timer2Timer(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button9Click(Sender: TObject);
+    procedure EsconderHistorico1Click(Sender: TObject);
+    procedure MostrarHistorico1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure ComboBox1Change(Sender: TObject);
+    procedure edt1Exit(Sender: TObject);
+    procedure edt2Exit(Sender: TObject);
+    procedure Button10Click(Sender: TObject);
+  private
+    function somar(n1, n2, n3: currency): currency;
+    function subtrair(n1, n2, n3: currency): currency;
+    function multiplicar(n1, n2, n3: currency): currency;
+    function dividir(n1, n2, n3: currency): currency;
+    function raizquadrada(n1, n2: currency): currency;
+    function potencia(n1, n2, n3: currency): Extended;
+    function resto(n1, n2: integer): Currency;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  principal: Tprincipal;
+  num1 : currency;
+  num2 : currency;
+  num3 : currency;
+
+
+implementation
+
+{$R *.dfm}
+
+procedure Tprincipal.Button10Click(Sender: TObject);
+begin
+
+memo1.Lines.Add(CurrToStr(num1)+ ' ' + CurrToStr(num1)+ ' ' + CurrToStr(num1))
+
+end;
+
+procedure Tprincipal.Button1Click(Sender: TObject);
+begin
+  edtresult.Text := CurrToStr(somar(StrToCurr(edt1.Text), StrToCurr(edt2.Text),
+    StrToCurr(edtresult.Text)));
+  if subtrair(StrToCurr(edtresult.Text), StrToCurr(edt1.Text), 0)
+    = StrToCurr(edt2.Text) then
+    Memo1.Lines.Add(edt1.Text + ' + ' + edt2.Text + ' = ' + edtresult.Text)
+  else
+    Memo1.Lines.Add(edt1.Text + ' + ' +
+      CurrToStr(subtrair(StrToCurr(edtresult.Text), StrToCurr(edt1.Text), 0)) +
+      ' = ' + edtresult.Text);
+end;
+
+procedure Tprincipal.Button2Click(Sender: TObject);
+begin
+  edtresult.Text := CurrToStr(subtrair(StrToCurr(edt1.Text),
+    StrToCurr(edt2.Text), StrToCurr(edtresult.Text)));
+  if somar(StrToCurr(edtresult.Text), StrToCurr(edt1.Text), 0)
+    = StrToCurr(edt2.Text) then
+    Memo1.Lines.Add(edt1.Text + ' - ' + edt2.Text + ' = ' + edtresult.Text)
+  else
+    Memo1.Lines.Add(edt1.Text + ' - ' +
+      CurrToStr(somar(StrToCurr(edtresult.Text), StrToCurr(edt1.Text), 0)) +
+      ' = ' + edtresult.Text);
+end;
+
+procedure Tprincipal.Button3Click(Sender: TObject);
+begin
+  edtresult.Text := CurrToStr(dividir(StrToCurr(edt1.Text),
+    StrToCurr(edt2.Text), StrToCurr(edtresult.Text)));
+  if multiplicar(StrToCurr(edtresult.Text), StrToCurr(edt1.Text), 0)
+    = StrToCurr(edt2.Text) then
+    Memo1.Lines.Add(edt1.Text + ' / ' + edt2.Text + ' = ' + edtresult.Text)
+  else
+    Memo1.Lines.Add(edt1.Text + ' / ' +
+      CurrToStr(multiplicar(StrToCurr(edtresult.Text), StrToCurr(edt2.Text), 0))
+      + ' = ' + edtresult.Text);
+end;
+
+procedure Tprincipal.Button4Click(Sender: TObject);
+begin
+  edtresult.Text := CurrToStr(multiplicar(StrToCurr(edt1.Text),
+    StrToCurr(edt2.Text), StrToCurr(edtresult.Text)));
+  if dividir(StrToCurr(edtresult.Text), StrToCurr(edt1.Text), 0)
+    = StrToCurr(edt2.Text) then
+    Memo1.Lines.Add(edt1.Text + ' * ' + edt2.Text + ' = ' + edtresult.Text)
+  else
+    Memo1.Lines.Add(CurrToStr(dividir(StrToCurr(edtresult.Text),
+      StrToCurr(edt2.Text), 0)) + ' * ' + edt2.Text + ' = ' + edtresult.Text);
+end;
+
+procedure Tprincipal.Button5Click(Sender: TObject);
+begin
+  Memo1.Clear;
+end;
+
+procedure Tprincipal.Button6Click(Sender: TObject);
+var
+  I: integer;
+begin
+  for I := 0 to 100 do
+  begin
+    Memo1.Lines.Add(IntToStr(I));
+
+  end;
+end;
+
+procedure Tprincipal.Button7Click(Sender: TObject);
+begin
+  Panel1.Visible := false;
+  principal.Height := 250;
+end;
+
+procedure Tprincipal.Button8Click(Sender: TObject);
+begin
+  case ComboBox1.ItemIndex of
+    0:
+      Button1.Click;
+    1:
+      Button2.Click;
+    2:
+      Button3.Click;
+    3:
+      Button4.Click;
+    4:
+      begin
+        edtresult.Text := CurrToStr(raizquadrada(StrToCurr(edt1.Text),
+          StrToCurr(edtresult.Text)));
+        if potencia(StrToCurr(edtresult.Text), 2, 0) = StrToCurr(edt1.Text) then
+          Memo1.Lines.Add(' ²√  ' + edt1.Text + ' = ' + edtresult.Text)
+        else
+          Memo1.Lines.Add
+            (' ²√  ' + CurrToStr(potencia(StrToCurr(edtresult.Text), 2, 0)) +
+            ' = ' + edtresult.Text);
+
+      end;
+    5:
+      begin
+
+        try
+
+          edtresult.Text := CurrToStr(potencia(StrToCurr(edt1.Text), 2,
+            StrToCurr(edtresult.Text)));
+          if raizquadrada(StrToCurr(edtresult.Text), 0) = StrToCurr(edt1.Text)
+          then
+            Memo1.Lines.Add(' x² ' + edt1.Text + ' = ' + edtresult.Text)
+          else
+            Memo1.Lines.Add
+              (' x²  ' + CurrToStr(raizquadrada(StrToCurr(edtresult.Text), 0)) +
+              ' = ' + edtresult.Text);
+
+        except
+
+          MessageDlg('Numero Maior Que o Suportado', mtError, [mbOK], 0);
+
+        end;
+      end;
+    6:
+      begin
+        edtresult.Text := CurrToStr(resto(strtoint(edt1.Text),strtoint(edt2.Text)));
+        Memo1.Lines.Add( edt1.text + ' %  ' + edt2.Text + ' = ' + edtresult.Text);
+      end;
+  else
+    MessageDlg('Você Devera Informar um Operador Aritmético', mtInformation,
+      [mbOK], 0);
+  end;
+end;
+
+procedure Tprincipal.Button9Click(Sender: TObject);
+begin
+  Panel1.Visible := true;
+  principal.Height := 700;
+end;
+
+procedure Tprincipal.ComboBox1Change(Sender: TObject);
+begin
+  //num1 := StrToCurr(edtb.Text);
+  //edtb.Clear ;
+  edtresult.Text := IntToStr(0);
+end;
+
+procedure Tprincipal.edt1Exit(Sender: TObject);
+begin
+  edt1.Visible := false;
+  edt2.Visible := true;
+  Label1.Caption := 'n2';
+end;
+
+procedure Tprincipal.edt2Exit(Sender: TObject);
+begin
+  edt2.Visible := false;
+  edt1.Visible := true;
+  Label1.Caption := 'n1';
+end;
+
+procedure Tprincipal.EsconderHistorico1Click(Sender: TObject);
+begin
+  Panel1.Visible := false;
+  principal.Height := 270;
+end;
+
+procedure Tprincipal.FormShow(Sender: TObject);
+begin
+  Label7.Caption := DateToStr(now);
+  Label9.Caption := TimeToStr(now);
+end;
+
+procedure Tprincipal.MostrarHistorico1Click(Sender: TObject);
+begin
+  Panel1.Visible := true;
+  principal.Height := 700;
+end;
+
+function Tprincipal.multiplicar(n1, n2, n3: currency): currency;
+begin
+  if n3 = 0 then
+    result := n1 * n2
+  else
+    result := n3 * n2;
+end;
+
+function Tprincipal.dividir(n1, n2, n3: currency): currency;
+begin
+  if n3 = 0 then
+    result := n1 / n2
+  else
+    result := n3 / n2;
+end;
+
+function Tprincipal.potencia(n1, n2, n3: currency): Extended;
+begin
+  if n3 = 0 then
+    result := power(n1, n2)
+  else
+    result := power(n3, n1);
+end;
+
+function Tprincipal.raizquadrada(n1, n2: currency): currency;
+begin
+  if n2 = 0 then
+    result := sqrt(n1)
+  else
+    result := sqrt(n2);
+end;
+
+function Tprincipal.resto(n1, n2: integer): Currency;
+begin
+  result := n1 mod n2;
+end;
+
+function Tprincipal.somar(n1, n2, n3: currency): currency;
+begin
+  if n3 = 0 then
+    result := n1 + n2
+  else
+    result := n1 + n3;
+end;
+
+function Tprincipal.subtrair(n1, n2, n3: currency): currency;
+begin
+  if n3 = 0 then
+    result := n1 - n2
+  else
+    result := n3 - n1;
+end;
+
+procedure Tprincipal.Timer1Timer(Sender: TObject);
+begin
+  Label7.Caption := DateToStr(now);
+  Label9.Caption := TimeToStr(now);
+end;
+
+procedure Tprincipal.Timer2Timer(Sender: TObject);
+var
+  contador: integer;
+begin
+  contador := Memo1.Lines.Count;
+  if contador <= 100 then
+    ProgressBar1.Position := Memo1.Lines.Count
+  else
+  begin
+    Memo1.Lines.Clear;
+    MessageDlg('Limite do Historico Atingido, Limpando', mtConfirmation,
+      [mbOK], 0);
+  end;
+
+end;
+
+end.
